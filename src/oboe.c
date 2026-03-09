71b7f78acadad576df3d9e6c51780680640999d4
NekoPunch! <nekopunch@peropero.net>
2026-03-09 10:30:57 +0800

    android

---
+++ src/oboe.c
@@ -0,0 +1,5 @@
+//
+// Created by Administrator on 2026/3/9.
+//
+
+#include "oboe.h"
---
+++ src/oboe.h
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 2026/3/9.
+//
+
+#ifndef AUDIORENDERER_OBOE_H
+#define AUDIORENDERER_OBOE_H
+
+
+class oboe
+{
+};
+
+
+#endif //AUDIORENDERER_OBOE_H
